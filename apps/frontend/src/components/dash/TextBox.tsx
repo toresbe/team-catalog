d66543006fd7d41e67044b12da6ccc3fb9f8c99d
Andreas Harnes <65889446+aharnes-nav@users.noreply.github.com>
2022-06-21 11:06:13 +0200

    Update TextBox.tsx

--- apps/frontend/src/components/dash/TextBox.tsx
+++ apps/frontend/src/components/dash/TextBox.tsx
@@ -20,20 +20,31 @@ const cardShadow = {
   },
 }
 
+const iconStyle = css({
+  height: '4rem',
+  width: '4rem',
+})
+
 export const TextBox = (props: { title: string; value: string | number; icon?: string; subtext?: string }) => {
   return (
     <Card overrides={cardShadow}>
       <Block display="flex" flexDirection="column" alignItems="center" justifyContent="space-around" width="15rem" height={theme.sizing.scale4800}>
         {/* {props.icon && <FontAwesomeIcon icon={props.icon} size="2x" color={theme.colors.accent300} />} */}
-        {props.icon && <img src={props.icon} />}
+        {props.icon && <img className={iconStyle} src={props.icon} />}
 
-        <LabelLarge color={'#005077'} $style={{ textAlign: 'center' }}>
+        <LabelLarge color={'#005077'} $style={{ textAlign: 'center', marginTop: '1rem' }}>
           {props.title}
         </LabelLarge>
         <LabelLarge className={valueStyle} $style={{ fontSize: '2.5em' }}>
           {props.value}
         </LabelLarge>
-        <label>{props.subtext}</label>
+        {props.subtext ? (
+          <label>{props.subtext}</label>
+        ) : (
+          <label role="hidden" style={{ color: '#ffffff' }}>
+            Easter egg
+          </label>
+        )}
       </Block>
     </Card>
   )
