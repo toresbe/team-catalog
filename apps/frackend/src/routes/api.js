d764aac9fbfe83b776f9e187dfcffba808cca987
Andreas Harnes <65889446+aharnes-nav@users.noreply.github.com>
2022-07-14 13:21:06 +0200

    Update api.js

--- apps/frackend/src/routes/api.js
+++ apps/frackend/src/routes/api.js
@@ -10,6 +10,16 @@ const teamsApi = apiUrl + '/team?status=ACTIVE'
 const clustersApi = apiUrl + '/cluster?status=ACTIVE'
 const productareasApi = apiUrl + '/productarea?status=ACTIVE'
 
+// interface TableStructure {
+//   name: String;
+//   team?: { name: String, id?: String };
+//   area?: { name: String, id?: String };
+//   cluster?: { name: String, id?: String };
+//   role: String;
+//   other: String;
+//   type: String;
+// }
+
 function getMemberIdents(teamcatEntity) {
   let members = new Set([])
   const teamArray = teamcatEntity.data.content
@@ -73,6 +83,117 @@ async function getFromNom(req, idents) {
     })
 }
 
+// {
+//   name: String;
+//   team?: { name: String, id?: String };
+//   area?: { name: String, id?: String };
+//   cluster?: { name: String, id?: String };
+//   role: String;
+//   other: String;
+//   type: String;
+// }
+
+function getData() {}
+
+function createTableData(teams, clusters, areas) {
+  let tableData = new Array()
+
+  teams.forEach((team) => {
+    if (team.members.length != 0) {
+      team.members.forEach((member) => {
+        let productAreaName
+        let clusterData = ''
+
+        if (team.productAreaId) {
+          productAreaName = getName(areas, team.productAreaId)
+        }
+        if (team.clusterIds.length != 0) {
+          let clusterData = getClusters(team.clusterIds, clusters)
+        }
+
+        let memberObject = {
+          name: { name: member.resource.fullName, ident: member.navIdent },
+          team: { name: team.name, id: team.id },
+          area: { name: productAreaName, id: team.productAreaId },
+          cluster: clusterData,
+          roles: member.roles.join(', '),
+          other: '',
+          type: member.resource.resourceType,
+        }
+
+        // console.log(memberObject)
+        tableData.push(memberObject)
+      })
+    }
+  })
+
+  areas.forEach((area) => {
+    if (area.members.length != 0) {
+      area.members.forEach((member) => {
+        let clusterData = ''
+
+        let memberObject = {
+          name: { name: member.resource.fullName, ident: member.navIdent },
+          team: { name: '', id: '' },
+          area: { name: area.name, id: area.id },
+          cluster: { name: '', id: '' },
+          roles: member.roles.join(', '),
+          other: '',
+          type: member.resource.resourceType,
+        }
+
+        // console.log(memberObject)
+        tableData.push(memberObject)
+      })
+    }
+  })
+
+  clusters.forEach((cluster) => {
+    if (cluster.members.length != 0) {
+      cluster.members.forEach((member) => {
+        let clusterData = ''
+
+        let memberObject = {
+          name: { name: member.resource.fullName, ident: member.navIdent },
+          team: { name: '', id: '' },
+          area: { name: '', id: '' },
+          cluster: { name: cluster.name, id: cluster.id },
+          roles: member.roles.join(', '),
+          other: '',
+          type: member.resource.resourceType,
+        }
+
+        // console.log(memberObject)
+        tableData.push(memberObject)
+      })
+    }
+  })
+  console.log(tableData.length)
+  return tableData
+}
+
+function getClusters(clusterIds, clusters) {
+  let clusterArray = new Array()
+
+  clusterIds.forEach((clusterId) => {
+    let clusterName = getName(clusters, clusterId)
+    let clusterData = { name: clusterName, id: clusterId }
+
+    clusterArray.push(clusterData)
+  })
+  return clusterArray
+}
+
+function getName(list, id) {
+  let name = ''
+  list.forEach((element) => {
+    if (element.id === id) {
+      name = element.name
+    }
+  })
+  return name
+}
+
 function setupApi(app) {
   app.get(
     '/api/teamcatTabell',
@@ -90,10 +211,18 @@ function setupApi(app) {
       const idents = getAllTeamCatIdents(teams, clusters, areas)
 
       const nomRessources = await getFromNom(req, idents)
+
+      const tableData = createTableData(
+        teams.data.content,
+        clusters.data.content,
+        areas.data.content
+      )
       res.send({
-        len: idents.length,
-        out: idents,
-        nomRessources: nomRessources,
+        // len: idents.length,
+        // out: idents,
+        // nomRessources: nomRessources.data,
+        teams: areas.data.content,
+        // tesst: 'test',
       })
     }
   )
